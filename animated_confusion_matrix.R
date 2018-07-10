fff75e5d722bd3a836b94950d046230870cc9abd
Nick Strayer <nick.strayer@gmail.com>
2018-07-10 10:23:55 -0500

    Attempted to overlay the training curves as well on the animation but it doesn't seem to be supported in gganimate :(

--- animated_confusion_matrix.R
+++ animated_confusion_matrix.R
@@ -1,7 +1,21 @@
 library(gganimate)
-performanceByEpoch <- read_rds('epoch_perf.rds')
+library(patchwork)
 
-performanceByEpoch %>% 
+historyDf <- read_rds("trainHistory.rds") %>% 
+  as.data.frame() %>% 
+  filter(epoch < 10)
+
+performanceByEpoch <- read_rds('epoch_perf.rds') %>% 
+  filter(epoch < 10)
+
+lossAccuracy <- historyDf %>% 
+  ggplot(aes(x = epoch, y = value, color = data)) +
+  geom_line(size = 0.2) +
+  geom_point(size = 0.4) +
+  facet_grid(metric~., scales = "free_y") +
+  theme_minimal()
+
+confusionMatrix <- performanceByEpoch %>% 
   ggplot(aes(x = truth,  y = predicted)) +
   geom_point(aes(size = count, color = good)) +
   geom_text(aes(label = count), 
@@ -9,7 +23,11 @@ performanceByEpoch %>%
             nudge_x = 0.1, nudge_y = 0.1) + 
   guides(color = FALSE, size = FALSE) +
   theme_minimal() +
-  labs(title = 'Epoch: {current_frame}') +
-  transition_manual(epoch) -> animatedPerformance
+  labs(title = 'Epoch: {current_frame}')
+
+
+(lossAccuracy / confusionMatrix) * transition_manual(epoch) -> animatedPerformance
 
 animate(animatedPerformance, nframes = length(unique(performanceByEpoch$epoch)))
+
+trainHistory 
